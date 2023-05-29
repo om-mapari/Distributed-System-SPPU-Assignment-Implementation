934c8092e7b58c40165267fcf5486885a251b117
Om Mapari <mapariom05@gmail.com>
2023-05-29 05:46:34 +0530

    client updated

--- Assignment 4/berkley-client.py
+++ Assignment 4/berkley-client.py
@@ -58,3 +58,18 @@ def initiateSlaveClient(port = 8080):
 if __name__=="__main__":
     # Initialize the slave/client
     initiateSlaveClient(port = 8080)
+
+
+'''
+The provided code represents a basic implementation of a client process in Python. It acts as a client connecting to the clock server and sends its local time to the server while receiving synchronized time from the server. Here's a summary of the main functions and their purposes:
+
+startSendingTime(slave_client): This function runs in a separate thread and continuously sends the current local time of the client to the clock server. It uses the datetime.datetime.now() function to get the current time and sends it as a string to the server using the send method of the slave_client socket. It sleeps for 5 seconds before sending the next time update.
+
+startReceivingTime(slave_client): This function also runs in a separate thread and continuously receives synchronized time from the clock server. It uses the recv method of the slave_client socket to receive data from the server. The received data is decoded, parsed into a datetime object using parser.parse from the dateutil library, and printed as the synchronized time at the client. This process repeats indefinitely.
+
+initiateSlaveClient(port): This function is the entry point for the client process. It creates a socket using socket.socket(), connects it to the clock server at the specified IP address ('127.0.0.1') and port number, and starts two separate threads for sending time and receiving synchronized time from the server.
+
+The client process continuously sends its local time to the server and receives synchronized time from the server, ensuring that its clock remains synchronized with the clock server.
+
+It's important to note that this code snippet provides a basic implementation of a client process. Depending on your specific requirements, you may need to modify and enhance the code to handle error cases, implement additional functionalities, or address any security considerations.
+'''
